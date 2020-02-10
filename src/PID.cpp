f516189b727e92647cb51e54ee509a506a2e51ab
adam-ubuntu <gy.adam90@gmail.com>
2020-02-10 15:22:47 -0800

    Initial version of UpdateError() function

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,16 +9,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  // Initialize PID coefficients
+  // Initialize PID coefficients and errors
   Kp = Kp_;
   Ki = Ki_;
   Kd = Kd_;
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+  // Update PID errors based on cte
+  double cte_prev = p_error;
+  p_error = Kp * cte;
+  i_error += Ki * cte;
+  d_error = Kd * (cte - cte_prev);
 }
 
 double PID::TotalError() {
